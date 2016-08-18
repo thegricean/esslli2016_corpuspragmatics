8231b80aaf964a7e565888f3c753e5a37d35796f
mhtess <mtessler@stanford.edu>
2016-08-18 15:43:44 +0200

    first draft of projection experiment

---
+++ experiment/js/sentences.js
@@ -0,0 +1,14 @@
+var sentences = [
+{
+	id: 1,
+	complement: "humans kill themselves",
+	sentence: "He knows that humans kill themselves.",
+	stimType: "cognitive"
+},
+{
+	id: 2,
+	complement: "the experiment was over",
+	sentence: "I was already intoxicated by the time I felt the stove was hot.",
+	stimType: "sense"
+}
+]
